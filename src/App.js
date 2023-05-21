df4a59c2779cab15251fa599d68059e3b2817b48
sanjana-19o8 <imt_2021107@iiitm.ac.in>
2023-05-21 21:12:24 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Navbar, Home, Footer} from './components/index';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      {/* <div className='home'>
+        <Home />
+      </div>
+      <div className='footer'>
+        <Footer />
+      </div> */}
     </div>
   );
 }
