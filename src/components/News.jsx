e97129faaafabbcab180e90c9e0072042f28e2ca
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-06-04 00:48:00 +0530

    updated News

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,24 +1,43 @@
 import React, { useState} from "react";
 import { Link } from "react-router-dom";
-import { Card, Row, Col, Typography, Avatar} from 'antd';
+import { Card, Row, Col, Typography, Avatar, Select} from 'antd';
 
 import { useGetNewsQuery } from "../services/newsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 import moment from "moment/moment";
 
 const { Title, Text} = Typography;
 
 const News = ( props) => {
-    const { data, isFetching} = useGetNewsQuery({ newsCategory: 'Cryptocurrency', count: props.simplified? 10:100});
+    const [ selectedCrypto, setSelectedCrypto] = useState('Cryptocurrency');
+    const { data: cryptoData} = useGetCryptosQuery(100);
+    const { data, isFetching} = useGetNewsQuery({ newsCategory: selectedCrypto, count: props.simplified? 5:100});
+
+    console.log(cryptoData);
     
     if(isFetching) return 'Loading...'
 
     return (
         <>
             {!props.simplified && 
+            <>
             <Title level={2}>
                 Latest Cryptocurrency News
-            </Title>}
-            <Row gutters={[24, 24]} className="crypto-card-container">
+            </Title>
+            <Select 
+                showSearch 
+                className="select-news" 
+                placeholder='Select a crypto' 
+                optionFilterProp="children" 
+                filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0} 
+                onChange={ val => setSelectedCrypto(val)}
+            >
+                <Select.Option value='Cryptocurrency'>Cryptocurrency</Select.Option>
+                {cryptoData?.data?.coins.map(coin => 
+                    <Select.Option value={coin.name}>{coin.name}</Select.Option>)}
+            </Select>
+            </>}
+            <Row gutters={[24, 24]}>
                 {data?.value?.map((news, i) => (
                         <Col xs={24} sm={12} lg={8} key={i}>
                             <Card 
@@ -31,7 +50,7 @@ const News = ( props) => {
                                     {news?.image?.thumbnail?.contentUrl && 
                                     <img src={news.image.thumbnail.contentUrl} alt="" className="news-image" />}
                                     <Text>
-                                        {news.description}
+                                        {news.description.length > 250 ? `${news.description.substring(0, 200)}...` : news.description}
                                     </Text>
                                 </a>
                                 <div className="provider-container">
