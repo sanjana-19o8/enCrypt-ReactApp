bc4bf8f430eabb1243529e8e4fff9b10abe22242
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-06-03 23:59:45 +0530

    update

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,9 @@
 import React, { useState} from "react";
 import { Link } from "react-router-dom";
-import { Card, Row, Col, Typography} from 'antd';
+import { Card, Row, Col, Typography, Avatar} from 'antd';
 
 import { useGetNewsQuery } from "../services/newsApi";
+import moment from "moment/moment";
 
 const { Title, Text} = Typography;
 
@@ -13,24 +14,33 @@ const News = ( props) => {
 
     return (
         <>
+            {!props.simplified && 
             <Title level={2}>
                 Latest Cryptocurrency News
-            </Title>
+            </Title>}
             <Row gutters={[24, 24]} className="crypto-card-container">
                 {data?.value?.map((news, i) => (
-                        <Col xs={24} sm={12} lg={8} className='news-card' key={i}>
+                        <Col xs={24} sm={12} lg={8} key={i}>
                             <Card 
+                            className='news-card'
                             title={news.name} 
                             hoverable={true} 
                             style={{ margin: '4px'}}
                             >
                                 <a href={news.url} target="_blank" rel="noreferrer">
                                     {news?.image?.thumbnail?.contentUrl && 
-                                    <img src={news?.image?.thumbnail?.contentUrl} alt="" className="news-image" />}
+                                    <img src={news.image.thumbnail.contentUrl} alt="" className="news-image" />}
                                     <Text>
                                         {news.description}
                                     </Text>
                                 </a>
+                                <div className="provider-container">
+                                    <div>
+                                        <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl} alt="" />
+                                        <Text className="provider-name">{news.provider[0]?.name}</Text>
+                                    </div>
+                                    <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                                </div>
                             </Card>
                         </Col>
                     )
