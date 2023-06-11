6a26227866022a61cd66fecb30319813c925ddb0
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-06-11 23:41:53 +0530

    update

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -8,17 +8,28 @@ const LineChart = ({ coinHistory, currentPrice, coinName}) => {
     const coinTimeStamp = [];
 
     for(let i=0; i< coinHistory?.data?.history?.length; i += 1){
-        coinPrice.oush(coinHistory?.data?.history[i].price);
+        coinPrice.push(coinHistory?.data?.history[i].price);
         coinTimeStamp.push(new Date(coinHistory?.data?.history[i].timestamp));
     }
     
     const data = {
+
+    }
+
+    const options = {
         
     }
 
     return (
         <>
-        LineChart
+        <Row className="chart-header">
+            <Typography.Title level={2} className="chart-title">{coinName} Price Chart</Typography.Title>
+            <Col className="price-container">
+                <Typography.Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Typography.Title>
+                <Typography.Title level={5} className="current-price">Current Price: {currentPrice}</Typography.Title>
+            </Col>
+        </Row>
+        <Line data={data} options={options} />
         </>
     )
 };
